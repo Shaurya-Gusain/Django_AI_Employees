df2b97190d880ca6881bce8492bf4a01758cabbd
Shaurya Gusain <shauryagusains@gmail.com>
2026-07-02 13:05:23 +0530

    fixed sse

--- support/views.py
+++ support/views.py
@@ -80,4 +80,7 @@ def conversation_stream(request,conversation_id):
         finally:
             unsubscribe(conversation_id,q)
             
-    return StreamingHttpResponse(event_stream(conversation_id), content_type="text/event-stream")
+    response = StreamingHttpResponse(event_stream(conversation_id), content_type="text/event-stream")
+    response['Cache-Control'] = 'no-cache'
+    response['X-Accel-Buffering'] = 'no'
+    return response
